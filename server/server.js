630601fbe6b06ed5202db8cc777d0a8a72c59745
Issac <issac@Issacs-MacBook-Air.local>
2014-12-07 15:05:28 -0800

    modularization and rearrangement

--- server/server.js
+++ server/server.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var partials = require('express-partials');
-var bodyParser = require('body-parser');
-var bcrypt = require('bcrypt-nodejs');
 var session = require('express-session');
 var mongoose = require('mongoose');
 
@@ -12,10 +10,7 @@ require('../server/config/middleware.js')(app, express);
 
 app.set('view engine', 'html');
 app.use(partials());
-app.use(bodyParser.json());
-// Parse forms (signup/login)
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
 
 app.use(session({
@@ -24,11 +19,5 @@ app.use(session({
   saveUninitialized: true
 }));
 
-
-// app.get('*', function(req, res) {
-//   res.sendfile('../client/index.html');
-// });
-
-
-console.log('Untitled is listening on 8080');
+console.log('Your app is listening on 8080');
 app.listen(8080);
